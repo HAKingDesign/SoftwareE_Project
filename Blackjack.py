d6ff969f11e8eb4d403a6228f72110e0575290fd
HAKingDesign <62446357+HAKingDesign@users.noreply.github.com>
2021-04-24 10:20:37 -0500

    Add files via upload

--- Blackjack.py
+++ Blackjack.py
@@ -16,7 +16,7 @@ def main():
 #creates a list where each card as a suit and a value
 def CreateDeck():
     suits = ['hearts', 'diamonds', 'spades', 'clubs']
-    values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
+    values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace(Eleven)']
 
     deck = [Card(value, suit) for value in values for suit in suits]
     #shuffle the list and return it
@@ -50,22 +50,32 @@ def playGame():
         if int(choice) == 2:
             newCard = deck.pop()
             hand.append(newCard)
+            newCardValue = getValue(newCard.value)
+            total = total + newCardValue
+            #ace case: to check if an ace needs to be changed to its one point value
+            if total > 21:
+                total=0
+                ln=len(hand)
+                for i in range(ln):
+                    if hand[i].value=='Ace(Eleven)':
+                        hand[i].value='Ace(One)' 
+                for j in range(ln):
+                    total=total + getValue(hand[j].value)
             print("Your new hand:") 
             for i in range(len(hand)):
                 print(hand[i].value, hand[i].suit)
+            print("Total value =", total)
             
             #again need to get the cards real value to add it to the total
-            newCardValue = getValue(newCard.value)
-            total = total + newCardValue
-            print("Total value =", total)
+
         #if the player doesnt enter 2 then they stand 
         #   standing is useless without a dealer so we can add the dealer next time
         else:
             print("You stood with", total)
             break
-
+        
         #if the total is > 21 then the player busts
-        if total > 21:
+        if total > 21:    
             print("Sorry you busted")
         #if total is 21 then they got a blackjack
         if total == 21:
@@ -80,8 +90,10 @@ def getValue(card):
         value = 10
     elif card == "King":
         value = 10
-    elif card == "Ace":
+    elif card == "Ace(Eleven)":
         value = 11
+    elif card == "Ace(One)":
+        value = 1
     else:
         value = int(card)
 
